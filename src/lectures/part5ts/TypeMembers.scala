af6c81edf3ed8c3b592463e8eab0560bac5b464b
tinhot97@gmail.com <tinhot97@gmail.com>
2019-10-27 16:42:15 +0700

    making progress in learning scala

--- src/lectures/part5ts/TypeMembers.scala
+++ src/lectures/part5ts/TypeMembers.scala
@@ -1,8 +1,7 @@
 package lectures.part5ts
 
-/**
-  * Created by Daniel.
-  */
+// Lesson 3
+
 object TypeMembers extends App {
 
 
@@ -11,38 +10,47 @@ object TypeMembers extends App {
   class Cat extends Animal
 
   class AnimalCollection {
-    type AnimalType // abstract type member
+    // abstract type member
+    type AnimalType
     type BoundedAnimal <: Animal
     type SuperBoundedAnimal >: Dog <: Animal
+
+    // Alias type member
     type AnimalC = Cat
   }
 
+  // Abstract type member is usually used for The compiler to do type inference
   val ac = new AnimalCollection
-  val dog: ac.AnimalType = ???
+  // val dog: ac.AnimalType = ???
+  // val dog: ac.AnimalType = new Dog -> Illegal
 
-  //  val cat: ac.BoundedAnimal = new Cat
+  // val cat: ac.BoundedAnimal = new Cat -> Illegal
 
-  val pup: ac.SuperBoundedAnimal = new Dog
+  val pup: ac.SuperBoundedAnimal = new Dog // legal! Because SuperBoundedAnimal has bound >: Dog
   val cat: ac.AnimalC = new Cat
 
+
+  // Type aliases is often used when we have name collisions when a lot of package imported
   type CatAlias = Cat
   val anotherCat: CatAlias = new Cat
 
   // alternative to generics
+  // Abstract type members is sometimes used in API, that's similar to generics
   trait MyList {
     type T
     def add(element: T): MyList
   }
 
   class NonEmptyList(value: Int) extends MyList {
-    override type T = Int
+    override type T = Int // We assign Abstract type T explicitly by Int
     def add(element: Int): MyList = ???
   }
 
   // .type
+  // We can use some value's type as type alias
   type CatsType = cat.type
   val newCat: CatsType = cat
-  //  new CatsType
+  // new CatsType -> Illegal! Because, The compiler don't know the the constructor to be used in this case
 
   /*
     Exercise - enforce a type to be applicable to SOME TYPES only
@@ -66,12 +74,17 @@ object TypeMembers extends App {
 //  }
 
   // OK
-  class IntList(hd: Int, tl: IntList) extends MList {
-    type A = Int
+  class IntList(hd: Integer, tl: IntList) extends MList with ApplicableToNumbers {
+    type A = Integer
     def head = hd
     def tail = tl
   }
 
   // Number
   // type members and type member constraints (bounds)
+
+  /*
+  Explanation:
+    MList with ApplicableNumbers, with means "overridden by"
+   */
 }
